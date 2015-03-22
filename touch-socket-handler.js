eaa5602281a2de84a2ab154b759b9e913c614c69
mikekavouras <kavourasm@gmail.com>
2015-03-21 22:32:37 -0400

    move it

--- touch-socket-handler.js
+++ touch-socket-handler.js
@@ -2,7 +2,7 @@ function handleTouch(data) {
   if (data.state === 0) {
     drawOthersCircle(data);
   } else if (data.state === 1) {
-
+    moveCircle(data);
   } else if (data.state === 2) {
     console.log('REMOVE NEW');
     $('#' + data.id).remove();
@@ -20,4 +20,11 @@ function drawOthersCircle(data) {
     'left' : data.position.x
   }).show();
   $('body').append($div);
+}
+
+function moveCircle(data) {
+  $('#' + data.id).css({
+    'top' : data.position.y,
+    'left' : data.position.x
+  });
 }
